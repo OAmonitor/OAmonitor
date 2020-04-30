a6f866c93244aff375382c428676cde706aa2639
Barbara Vreede <b.vreede@gmail.com>
2020-04-30 14:48:00 +0200

    added source file to open in test

--- tests/testthat/test-open_everything.R
+++ tests/testthat/test-open_everything.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 6)
+context("open_everything")
+
+test_that("we can open an example file", {
+  library(readxl)
+  allfilesource <- system.file("extdata", "config_pub_files.xlsx", package = "OAmonitor")
+  allfiles <- readxl::read_excel(allfilesource)
+  open_everything(allfiles)
 })
