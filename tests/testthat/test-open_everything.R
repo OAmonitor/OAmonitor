414ed975eb73985da420f44c0922dccd3b8e6fbe
Barbara Vreede <b.vreede@gmail.com>
2020-05-05 13:13:00 +0200

    add tests on data

--- tests/testthat/test-open_everything.R
+++ tests/testthat/test-open_everything.R
@@ -1,9 +1,10 @@
 context("open_everything")
 
-test_that("opening example files", {
-  library(readxl)
+test_that("can open example data correctly", {
   allfilesource <- system.file("extdata", "config_pub_files.xlsx", package = "OAmonitor")
   datafiles <- system.file("extdata", "", package = "OAmonitor")
   df <- open_everything(file = allfilesource, dir = datafiles)
   expect_true("tbl" %in% class(df))
+  expect_true(nrow(df) == 1136)
+  expect_true(ncol(df) == 17)
 })
